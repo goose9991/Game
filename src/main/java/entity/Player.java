20770106d825764121a8ea899bd9b206d8f28f5d
Gus Garay <gusgaray9@gmail.com>
2025-04-30 14:55:31 -0500

    updated sprites

--- src/main/java/entity/Player.java
+++ src/main/java/entity/Player.java
@@ -47,14 +47,14 @@ public class Player extends Entity{
 
     public void getPlayerImg(){
 
-        up1 = setup("/player/joselito_up_1");
-        up2 = setup("/player/joselito_up_2");
-        down1 = setup("/player/joselito_down_1");
-        down2 = setup("/player/joselito_down_2");
-        left1 = setup("/player/joselito_left_1");
-        left2 = setup("/player/joselito_left_2");
-        right1 = setup("/player/joselito_right_1");
-        right2 = setup("/player/joselito_right_2");
+        up1 = setup("/player/joselito_up1");
+        up2 = setup("/player/joselito_up2");
+        down1 = setup("/player/joselito_down1");
+        down2 = setup("/player/joselito_down2");
+        left1 = setup("/player/joselito_left1");
+        left2 = setup("/player/joselito_left2");
+        right1 = setup("/player/joselito_right1");
+        right2 = setup("/player/joselito_right2");
 
     }
 
